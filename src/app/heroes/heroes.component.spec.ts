832e6bf6be6c6fa65096495959065ebd0431e6fd
Ian Seng <ianseng@outlook.com>
2020-04-19 22:06:19 -0400

    testing a component with mocking to isolate code

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,31 @@
+import { HeroesComponent } from "./heroes.component";
+import { of } from "rxjs";
+
+describe('HeroesComponent', () => {
+    let component: HeroesComponent;
+    let HEROES;
+    let mockHeroService;
+
+    beforeEach(() => {
+        HEROES = [
+            { id: 1, name: 'spider', strength: 8 },
+            { id: 2, name: 'wonder woman', strength: 25 },
+            { id: 3, name: 'super man', strength: 108 },
+        ];
+
+        mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero'])
+
+        component = new HeroesComponent(mockHeroService);
+    })
+
+    describe('deleted', () => {
+        it ('should remove the indicated hero from the heroes list', () => {
+            mockHeroService.deleteHero.and.returnValue(of(true))
+            component.heroes = HEROES;
+
+            component.delete(HEROES[2]);
+
+            expect(component.heroes.length).toBe(2);
+        })
+    })
+})
